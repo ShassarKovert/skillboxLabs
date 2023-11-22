cc10799f6c1a1ed09f25bdd9b60366ccfe486013
ShassarKovert <65663689+ShassarKovert@users.noreply.github.com>
2023-11-22 19:17:25 +0300

    Add files via upload

---
+++ homework5/assign2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace another_test
+{
+    internal class Program
+    {
+        static string[] SplitText(string text)
+        {
+            var delimeter = ' ';
+            return text.Split(delimeter);
+        }
+        static void Reverse(string getString)
+        {
+            string[] Splitted = SplitText(getString);
+            string ResultString="", NewString;
+                for (int i = Splitted.Length - 1; i >= 0; i--)
+            {
+                NewString = Splitted[i];
+                ResultString += NewString + " ";
+            }
+                Console.WriteLine(ResultString);
+        }
+
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Введите строку: ");
+           string String = Console.ReadLine();
+           Reverse(String);
+        }
+    }
+}
