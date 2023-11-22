55f27f233f41f4eeb6de6d75f5565af35cb2da98
ShassarKovert <65663689+ShassarKovert@users.noreply.github.com>
2023-11-22 19:01:11 +0300

    Add files via upload

---
+++ homework5/assign2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace another_test
+{
+    internal class Program
+    {
+        static string[] SplitText(string text)
+        {
+            var delimeter = ' ';
+            return text.Split(delimeter);
+        }
+
+        static void Print(string[] args)
+        { 
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Введите строку: ");
+           string String = Console.ReadLine();
+           Print(SplitText(String));
+        }
+    }
+}
